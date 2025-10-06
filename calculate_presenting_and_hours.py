d6c0bcf642ee917ae8a9302ee680d1e20e665894
Aliaksandr Samsonik <44325769+AlexSamsonik@users.noreply.github.com>
2025-10-06 16:19:07 +0200

    Update main(). Removed unused function and unit test. (#4)

--- calculate_presenting_and_hours.py
+++ calculate_presenting_and_hours.py
@@ -1,48 +1,13 @@
 """There is main module to calculate actual the actual number of hours children are in kindergarten."""
 
-import logging
 from argparse import ArgumentParser
-from dataclasses import dataclass
 from os import environ
 
 from dotenv import load_dotenv
 from playwright.sync_api import sync_playwright
 
 from src.collected_data_from_ui import run
-from src.helpers import get_first_and_last_day_of_month, calculate_interval_hours
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(format="%(asctime)s [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)", level=logging.INFO)
-
-
-@dataclass
-class ActualPresent:
-    """Dataclass to storage actual presenting children in kindergarten."""
-
-    num: str
-    sur_name: str
-    first_name: str
-    unit: str
-    day: str
-    start_time: str
-    end_time: str
-
-
-def print_calculation(first_name: str, presenting: list[ActualPresent]) -> None:
-    """Calculate and log the total days, meal payments, hours, and hour payments for a child in kindergarten.
-
-    :param first_name: The first name of the child.
-    :param presenting: A list of ActualPresent objects representing the child's attendance.
-    """
-    hours = sum([(calculate_interval_hours(i.start_time, i.end_time)) for i in presenting])
-
-    logger.info(f"{first_name}. Days in kindergarten: '{len(presenting)}'.")
-    rounded_meals_payment = round(len(presenting) * int(environ["PAYMENT_FOR_MEALS_FOR_DAY"]), 2)
-    logger.info(f"{first_name}. Payment for meals: '{'{:.2f}'.format(rounded_meals_payment)}' zl.")
-
-    logger.info(f"{first_name}. Hours in kindergarten: '{hours}'.")
-    rounded_hours_payment = round(hours * float(environ["PAYMENT_FOR_HOUR"]), 2)
-    logger.info(f"{first_name}. Payment for hours: '{'{:.2f}'.format(rounded_hours_payment)}' zl.")
+from src.helpers import print_calculation
 
 
 def main(month: str, year: str):
@@ -52,16 +17,11 @@ def main(month: str, year: str):
     :param year: The year for which the calculation is to be made. If not specified, the current year is used.
     """
     load_dotenv()
-    first_day, last_day = get_first_and_last_day_of_month(month, year)
     with sync_playwright() as playwright:
-        presents = run(playwright, first_day, last_day)
+        presents = run(playwright, month, year)
 
-    actual_presents: list[ActualPresent] = [ActualPresent(*presents[i : i + 7]) for i in range(0, len(presents), 7)]
-    wa_present: list[ActualPresent] = [ap for ap in actual_presents if ap.first_name == environ["WA_FIRST_NAME"]]
-    we_present: list[ActualPresent] = [ap for ap in actual_presents if ap.first_name == environ["WE_FIRST_NAME"]]
-
-    print_calculation(environ["WA_FIRST_NAME"], wa_present)
-    print_calculation(environ["WE_FIRST_NAME"], we_present)
+    print_calculation(environ["WA_FIRST_NAME"], presents[environ["WA_ID"]])
+    print_calculation(environ["WE_FIRST_NAME"], presents[environ["WE_ID"]])
 
 
 if __name__ == "__main__":
---
+++ tests/test_unit.py
@@ -0,0 +1,46 @@
+"""There are tests for helpers functions."""
+
+import pytest
+
+from src.helpers import calculate_interval_hours
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@pytest.mark.parametrize(
+    "start_time, end_time, expected_hours",
+    [
+        ("07:59", "08:00", 1),
+        ("07:00", "08:00", 1),
+        ("06:30", "07:45", 2),
+        ("08:00", "13:00", 0),
+        ("08:00", "13:01", 1),
+        ("12:59", "13:01", 1),
+        ("13:00", "14:00", 1),
+        ("13:01", "15:00", 2),
+        ("07:00", "14:00", 2),
+        ("07:00", "15:00", 3),
+        ("07:59", "13:01", 2),
+        ("07:59", "14:00", 2),
+        ("07:30", "08:30", 1),
+        ("12:30", "14:30", 2),
+        ("13:59", "15:59", 2),
+        ("06:00", "16:00", 5),
+        ("07:00", "13:00", 1),
+        ("13:00", "13:01", 1),
+        ("07:59", "08:01", 1),
+        ("12:59", "13:02", 1),
+        ("08:04", "14:09", 2),
+        ("14:31", "14:35", 1),
+    ],
+)
+def test_calculate_interval_hours(start_time, end_time, expected_hours):
+    """Test validate correctly calculates the total hours outside the core time interval.
+
+    :param start_time: Start time in the format "HH:MM", representing the beginning of the interval.
+    :param end_time: End time in the format "HH:MM", representing the end of the interval.
+    :param expected_hours: Expected total hours outside the core time interval, used for assertion.
+    :return: None. Asserts the function output against expected_hours.
+    """
+    assert calculate_interval_hours(start_time, end_time) == expected_hours
