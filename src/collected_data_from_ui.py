c6491f4cbe77db53bca86d637ca8c18df007a9e3
Aliaksandr Samsonik <aliaksandr_samsonik@epam.com>
2025-10-06 14:08:42 +0200

    'Collected data from UI' has been adopt for the new UI.

--- src/collected_data_from_ui.py
+++ src/collected_data_from_ui.py
@@ -2,23 +2,38 @@
 
 import logging
 from os import environ
-
+from time import sleep
 from playwright.sync_api import Playwright
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format="%(asctime)s [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)", level=logging.INFO)
 
+children_ids = [environ["WA_ID"], environ["WE_ID"]]
 
-def run(playwright: Playwright, date_from: str, date_to: str):
+
+def select_element(locator, option, page) -> None:
+    """Select option in selector and waiting couple of seconds.
+
+    :param locator: Locator for selector element.
+    :param option: Option for selector.
+    :param page: Page object.
+    :return: None
+    """
+    page.locator(locator).select_option(option)
+    sleep(int(environ["WAITING_AFTER_SELECT"]))  # page.wait_for_load_state() is to fast, need to waiting 3 seconds.
+
+
+def run(playwright: Playwright, month: str, year: str) -> dict:
     """Running Playwright and return list with all presented information for specify date range.
 
     :param playwright: Playwright instance.
-    :param date_from: Start search date. Date format must be 'YYYY-MM-DD'.
-    :param date_to: End search date. Date format must be 'YYYY-MM-DD'.
-    :return:
+    :param month: Number of month from 1 to 12. If not specified, the current month is used.
+    :param year: The year for which the calculation is to be made. If not specified, the current year is used.
+
+    :return: Dictionary with list of dictionary with start_time and end_time.
     """
     logger.info("Playwright launch.")
-    browser = playwright.firefox.launch()
+    browser = playwright.firefox.launch()  # launch(headless=False) for debugging
     page = browser.new_page()
 
     # Login to the kindergarten application
@@ -32,35 +47,30 @@ def run(playwright: Playwright, date_from: str, date_to: str):
     page.locator("xpath=//*[@type='submit']").click()
     page.wait_for_load_state()
 
-    # Input search dates
-    logger.info(f"Input 'Data od' equals '{date_from}'.")
-    page.get_by_placeholder("Data od").fill(date_from)
-    logger.info(f"Input 'Data do' equals '{date_to}'.")
-    page.get_by_placeholder("Data do").fill(date_to)
-    logger.info("Click 'Szukaj' button.")
-    page.locator("xpath=//*[@type='submit']").click()
-    page.wait_for_load_state()
+    logger.info("Open 'Obecnosci' page.")
+    page.locator("xpath=//*[@id='btn_3']/button").click()
 
-    # Count page in table
-    pagination = page.locator("xpath=//ul[@class='pagination']").text_content()
-    try:
-        pages_in_table = int(pagination.replace("»", "")[-1:])
-        logger.info(f"Found '{pages_in_table}' pages in 'Obecności' table.")
-    except ValueError:
-        pages_in_table = 0
-        logger.info(f"Found '{pages_in_table}' pages in 'Obecności' table.")
+    presents = {}
+    logger.info("Start collect information about actual presenting children in kindergarten.")
+    for child_id in children_ids:
+        presents.update({child_id: []})
 
-    presents = []
-    if pages_in_table:
-        logger.info("Start collect information about actual presenting children in kindergarten.")
-        for i in range(pages_in_table):
-            rows = page.locator("//table[@class='table table-bordered']/tbody/tr/td")
-            for j in range(rows.count()):
-                presents.append(rows.nth(j).text_content().strip())
-            if i != pages_in_table - 1:
-                logger.info("Click '»' button.")
-                page.locator("xpath=//li[@class='PagedList-skipToNext']/a[@rel='next']").click()
-            page.wait_for_load_state()
+        logger.info("Wybierz dziecko.")
+        select_element(locator="xpath=//*[@id='MasterComboDziecko']", option=child_id, page=page)
+
+        logger.info("Wybierz Miesiac.")
+        select_element(locator="xpath=//*[@id='comboMiesiac']", option=month, page=page)
+
+        logger.info("Wybierz Rok.")
+        select_element(locator="xpath=//*[@id='comboRok']", option=year, page=page)
+
+        rows = page.locator("xpath=//*[@id='table_obecnosci']/tbody/tr/td")
+        for j in range(rows.count()):
+            logger.info("%s. Collecting...", j + 1)
+            if len(rows.nth(j).text_content()) in [11, 12]:
+                start_time = rows.nth(j).locator("xpath=/div[@class='overlay']/div[@class='wejscie']").text_content()
+                end_time = rows.nth(j).locator("xpath=/div[@class='overlay']/div[@class='wyjscie']").text_content()
+                presents[child_id].append({"start_time": start_time, "end_time": end_time})
 
     browser.close()
     return presents
