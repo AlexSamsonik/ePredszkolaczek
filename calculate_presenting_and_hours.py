f0bb2f3f12fc7cd6223002b93604c91307c413e1
Aliaksandr Samsonik <aliaksandr_samsonik@epam.com>
2025-10-10 17:41:45 +0200

    main() has been updated to use api request.

--- calculate_presenting_and_hours.py
+++ calculate_presenting_and_hours.py
@@ -6,8 +6,11 @@ from os import environ
 from dotenv import load_dotenv
 from playwright.sync_api import sync_playwright
 
+from src.collected_data_from_api import get_obecnosci
 from src.collected_data_from_ui import run
-from src.helpers import print_calculation
+from src.helpers import print_calculation, compose_string_from_dict_list, extract_entrance_exit_times
+
+load_dotenv()
 
 
 def main(month: str, year: str):
@@ -16,12 +19,22 @@ def main(month: str, year: str):
     :param month: Number of month from 1 to 12. If not specified, the current month is used.
     :param year: The year for which the calculation is to be made. If not specified, the current year is used.
     """
-    load_dotenv()
     with sync_playwright() as playwright:
-        presents = run(playwright, month, year)
+        cookies = run(playwright, headless=bool(int(environ["HEADLESS_MODE"])))
 
-    print_calculation(environ["WA_FIRST_NAME"], presents[environ["WA_ID"]])
-    print_calculation(environ["WE_FIRST_NAME"], presents[environ["WE_ID"]])
+    # Getting cookies for API request
+    cookies_for_request = compose_string_from_dict_list(
+        dict_list=cookies,
+        field_order=["ADFSClientFedUtilCookie", "ADFSClientFedUtilCookie1", "__Host-ASP.NET_SessionId_Intendent"],
+    )
+    children = [
+        {"id": environ["WA_ID"], "first_name": environ["WA_FIRST_NAME"]},
+        {"id": environ["WE_ID"], "first_name": environ["WE_FIRST_NAME"]},
+    ]
+    for child in children:
+        obecnosci = get_obecnosci(child_id=child["id"], month=month, year=year, cookies=cookies_for_request)
+        presenting = extract_entrance_exit_times(attendance_data=obecnosci)
+        print_calculation(first_name=child["first_name"], presenting=presenting)
 
 
 if __name__ == "__main__":
